1d052f181922af7b4188aa6892ccbafe0580ebe3
xiaoqidun <xiaoqidun@gmail.com>
2021-04-15 13:39:32 +0800

    style(命名风格): 调整命名

--- gowsl.go
+++ gowsl.go
@@ -24,21 +24,21 @@ func Available() bool {
 }
 
 // Command 返回*exec.Cmd以执行指定的程序
-func Command(wsl string, name string, args ...string) *exec.Cmd {
-	if wsl == "" {
+func Command(distribution string, name string, args ...string) *exec.Cmd {
+	if distribution == "" {
 		args = append([]string{name}, args...)
 	} else {
-		args = append([]string{"-d", wsl, name}, args...)
+		args = append([]string{"-d", distribution, name}, args...)
 	}
 	return exec.Command(binary, args...)
 }
 
 // CommandContext 与Command类似，但包含一个上下文
-func CommandContext(ctx context.Context, wsl string, name string, args ...string) *exec.Cmd {
-	if wsl == "" {
+func CommandContext(ctx context.Context, distribution string, name string, args ...string) *exec.Cmd {
+	if distribution == "" {
 		args = append([]string{name}, args...)
 	} else {
-		args = append([]string{"-d", wsl, name}, args...)
+		args = append([]string{"-d", distribution, name}, args...)
 	}
 	return exec.CommandContext(ctx, binary, args...)
 }

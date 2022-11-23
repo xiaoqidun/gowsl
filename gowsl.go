9b7eb4ccd62da31629d54b665e4ed34d53b4554e
xiaoqidun <xiaoqidun@gmail.com>
2022-11-23 21:53:40 +0800

    feat(执行命令): 支持使用指定的用户在WSL发行版中执行命令

--- gowsl.go
+++ gowsl.go
@@ -40,21 +40,25 @@ func Registered(distribution string) bool {
 }
 
 // Command 返回*exec.Cmd以执行指定的程序
-func Command(distribution string, name string, args ...string) *exec.Cmd {
-	if distribution == "" {
-		args = append([]string{name}, args...)
-	} else {
-		args = append([]string{"-d", distribution, name}, args...)
+func Command(distribution string, user string, name string, args ...string) *exec.Cmd {
+	args = append([]string{name}, args...)
+	if user != "" {
+		args = append([]string{"-u", user}, args...)
+	}
+	if distribution != "" {
+		args = append([]string{"-d", distribution}, args...)
 	}
 	return exec.Command(binary, args...)
 }
 
 // CommandContext 与Command类似，但包含一个上下文
-func CommandContext(ctx context.Context, distribution string, name string, args ...string) *exec.Cmd {
-	if distribution == "" {
-		args = append([]string{name}, args...)
-	} else {
-		args = append([]string{"-d", distribution, name}, args...)
+func CommandContext(ctx context.Context, distribution string, user string, name string, args ...string) *exec.Cmd {
+	args = append([]string{name}, args...)
+	if user != "" {
+		args = append([]string{"-u", user}, args...)
+	}
+	if distribution != "" {
+		args = append([]string{"-d", distribution}, args...)
 	}
 	return exec.CommandContext(ctx, binary, args...)
 }
